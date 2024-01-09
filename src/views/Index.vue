7f120270e55934c8dec2f481f3c264531c92bfd6
Administrator <378715546@qq.com>
2024-01-09 22:15:46 +0800

    Changes

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,37 +1,39 @@
 <template>
   <div>
-<!--    <Head></Head>-->
     <div style="padding: 50px 20%">
-<!--      <SearchInput v-model="warningInfoSearchInput"-->
-<!--                   placeholder="请输入资源名称" hasBtn-->
-<!--                   @enter="warningInfoSearch"-->
-<!--      />-->
       <el-input
           v-model="warningInfoSearchInput"
           size="large"
           placeholder="请输入资源名称(回车搜索)"
           @keyup.native.enter="warningInfoSearch"
-          ></el-input>
+      ></el-input>
     </div>
     <div style="margin: 0 50px">
-      <el-tabs v-if="activeName" v-model="activeName" class="demo-tabs" @tab-change="handleChange">
-        <el-tab-pane v-for="tab in tabsList.value" :label="tab.split('&')[0]" :name="tab.split('&')[1]">
+      <!--      <el-tabs v-if="activeName" v-model="activeName" class="demo-tabs" @tab-change="handleChange">-->
+      <!--        <el-tab-pane v-for="tab in tabsList.value" :label="tab.split('&')[0]" :name="tab.split('&')[1]">-->
 
-          <el-card class="box-card" v-loading="isLoading">
-            <div v-for="item in tableData.value" class="text item">
-              <el-link class="text item" @click="goArticle(item.url)">{{ item.title }}</el-link>
-            </div>
-          </el-card>
+      <el-card class="box-card"  v-for="(tab,index) in tabsList.value" :key="index">
+        <div slot="header" class="clearfix" style="padding: 10px;">
+          <span>{{ tab.split('&')[0] }}</span>
+        </div>
+        <el-scrollbar height="500px" v-loading="isLoading[tab.split('&')[1]]">
+          <!--        <el-scrollbar :height="tableData.value[index]? '200px' : '250px'" >-->
+          <div class="text item" v-for="item in tableData[tab.split('&')[1]]" :key="item.id">
+            <el-link class="text item" @click="goArticle(item.url)">{{ item.title }}</el-link>
+          </div>
+        </el-scrollbar>
+      </el-card>
 
-        </el-tab-pane>
-      </el-tabs>
+
+      <!--        </el-tab-pane>-->
+      <!--      </el-tabs>-->
     </div>
 
   </div>
-<!--  <div style="clear:both">-->
-<!--    <Link/>-->
-<!--    <Footer/>-->
-<!--  </div>-->
+  <!--  <div style="clear:both">-->
+  <!--    <Link/>-->
+  <!--    <Footer/>-->
+  <!--  </div>-->
 </template>
 
 <script setup>
@@ -46,6 +48,7 @@ const route = useRoute()
 
 let activeName = ref('')
 let tabsList = reactive([])
+let isLoading = reactive({})
 onBeforeMount(() => {
   //获取导航列表
   proxy.$axios
@@ -60,35 +63,40 @@ onBeforeMount(() => {
 })
 
 
-let isLoading = ref(false)
 let warningInfoSearchInput = ref()
-let tableData = reactive([])
+let tableData = reactive({})
 
 //获取首页数据
-const getIndexData = (() => {
-  if (!warningInfoSearchInput.value){
+const getIndexData = ((active_name) => {
+  if (!warningInfoSearchInput.value) {
     return
   }
-  isLoading.value = true
+  console.log(active_name)
+  // isLoading.value = true
   proxy.$axios
       .get("/get_list",
           {
             keyword: warningInfoSearchInput.value,
-            activeName: activeName.value
+            activeName: active_name
           }
       )
       .then((res) => {
         //请求成功
-        tableData.value = res.data
+        tableData[active_name] = res.data
       })
       .finally(() => {
-        isLoading.value = false
+        // isLoading.value = false
+        isLoading[active_name] = false
       })
 })
 
 
 const warningInfoSearch = (() => {
-  getIndexData();
+  for (let tab = 0; tab < tabsList.value.length; tab++){
+    isLoading[tabsList.value[tab].split('&')[1]] = true
+  }
+  for (let tab = 0; tab < tabsList.value.length; tab++)
+    getIndexData(tabsList.value[tab].split('&')[1]);
 })
 
 //跳转文章详情页
@@ -129,7 +137,20 @@ html, body {
   padding: 10px 20px;
 }
 
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+
 .box-card {
+  float: left;
+  margin: 12px;
+  width: 30%;
 
 }
 </style>
